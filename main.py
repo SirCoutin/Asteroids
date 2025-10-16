9b7f03541e174a20633bdf20013a7001da086afe
SirCoutin <wafflez404@gmail.com>
2025-10-16 18:29:06 -0300

    Update
    Added:
    
    WASD Movement
    Player object

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from constants import *
+from player import *
 import pygame
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player.update(dt)
                 
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
